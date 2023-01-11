763caab1f1fe60f61202381abfa423ab0a36a1e6
JUNSUNG06 <forest2847@gmail.com>
2023-01-11 18:19:56 +0900

    한국, 일본 타일맵 수정

--- Assets/02.Scripts/Player/Slide.cs
+++ Assets/02.Scripts/Player/Slide.cs
@@ -7,24 +7,27 @@ public class Slide : MonoBehaviour
     public Vector2 slideColSize;
 
     private BoxCollider2D col;
+    private Vector2 originColSize;
+    private Vector2 originColOffset;
 
     private void Start()
     {
         col = GetComponent<BoxCollider2D>();
 
         GetComponent<PlayerInput>().slideAction += Slidding;
-    }
-
-    private void Update()
-    {
-        /*if(Input.GetMouseButton(1))
-        {
-            Slidding();
-        }*/
+        originColSize = col.size;
+        originColOffset = col.offset;
     }
 
     public void Slidding()
     {
         col.size = slideColSize;
+        col.offset = new Vector2(0, -(originColSize.y - slideColSize.y) / 2);
+    }
+
+    public void SetColliderOrigin()
+    {
+        col.size = originColSize;
+        col.offset = originColOffset;
     }
 }
