292af66ae67d9c1f408af330bc9d6d2cf928f563
JUNSUNG06 <forest2847@gmail.com>
2023-01-12 11:05:19 +0900

    플레이어 카메라

---
+++ Assets/02.Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Vector2 offset;
+    public GameObject player;
+
+    private float playerY;
+
+    private void Start()
+    {
+        playerY = player.transform.position.y;
+    }
+    private void Update()
+    {
+        transform.position = new Vector2(player.transform.position.x + offset.x, playerY + offset.y);
+    }
+}
